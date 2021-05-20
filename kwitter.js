62b1a1115fe7aa345aaa78fab47b2d182e4d9945
Manvimitta <77534496+Manvimitta@users.noreply.github.com>
2021-05-20 14:53:33 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,7 @@
+function addUser() {
+  user_name = document.getElementById("user_name").value;
+  password = document.getElementById("password").value;
+  localStorage.setItem("user_name", user_name);
+  localStorage.setItem("password", password);
+  window.location = kwitter_room.html;
+}
